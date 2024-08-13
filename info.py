ee82a9b40cc07a4691eff45d386abf4b480a8935
mrpanda2 <137524733+mrpanda2@users.noreply.github.com>
2024-08-13 23:40:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-SECONDDB_URI = environ.get('SECONDDB_URI', None)
+SECONDDB_URI = environ.get('SECONDDB_URI', 'mongodb+srv://Seconddbpanda:Seconddbpanda@seconddbpanda.re2px.mongodb.net/?retryWrites=true&w=majority&appName=Seconddbpanda')
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Dqfilterpanda:Dqfilterpanda@dqfilterpanda.1avdk.mongodb.net/?retryWrites=true&w=majority&appName=Dqfilterpanda")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Dqfilterpanda")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
